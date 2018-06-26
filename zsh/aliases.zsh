777d2383cbb50c480a85389758c5145cf30c5224
Mikkel Kofoed <mikkel.kofoed@gmail.com>
2018-06-26 10:29:49 +0200

    Added more alises and cleanup

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -29,16 +29,10 @@ alias grep='grep --color=auto'
 alias df='df -h' # disk free, in Gigabytes, not bytes
 alias du='du -h -c' # calculate disk usage for a folder
 
-# Applications
-alias ios='open -a /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone\ Simulator.app'
-
 # IP addresses
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en1"
 alias ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'"
-
-# Flush Directory Service cache
-alias flush="dscacheutil -flushcache"
 
 # View HTTP traffic
 alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
@@ -49,8 +43,10 @@ alias trimcopy="tr -d '\n' | pbcopy"
 
 # Recursively delete `.DS_Store` files
 alias cleanup="find . -name '*.DS_Store' -type f -ls -delete"
-# remove broken symlinks
+# Remove broken symlinks
 alias clsym="find -L . -name . -o -type d -prune -o -type l -exec rm {} +"
+# Remove .pyc-files in directory recursively
+alias nopyc="find . -name '*.pyc' -exec rm -f {} \;"
 
 # File size
 alias fs="stat -f \"%z bytes\""
@@ -83,10 +79,9 @@ alias aliases='vim ~/.dotfiles/zsh/aliases.zsh'
 # CD to dotfiles.
 alias dotfiles='cd ~/.dotfiles/'
 
+# Django shortcuts
 alias dr='dj runserver'
 alias ds='dj shell'
-
-# Remove .pyc-files in directory recursively
-alias nopyc="find . -name '*.pyc' -exec rm -f {} \;"
+alias dbs='dj dbshell'
 
 alias pcat='pygmentize -f terminal256 -O style=native -g'
